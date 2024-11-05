58c190aa9150b60c652d3521933706abdfc25dd6
Harsh Singh <hk5425901@gmail.com>
2024-11-05 16:45:26 +0530

    Added auth pages

---
+++ src/app/_component/Form.tsx
@@ -0,0 +1,68 @@
+"use client"
+
+import {useState } from "react";
+import Image from "next/image";
+import vakilGiriAuth from "@/app/assets/vakilgiri_auth.png"
+import Login from "./Login";
+import ForgetPassword from "./ForgetPassword";
+import Regester from "./Regester";
+
+const Form = () => {
+  const [login, setLogin] = useState<boolean>(true);
+  const [forgetPassword, setForgetPassword] = useState<boolean>(false);
+  const [regestration, setRegestration] = useState<boolean>(false);
+
+  const handleForgetPassword = () => {
+    setLogin(false);
+    setForgetPassword(true);
+  };
+
+  const handleBackToLogin = () => {
+      setForgetPassword(false);
+      setLogin(true);
+  };
+
+  const alreadyLogin = () => {
+      setRegestration(false);
+      setLogin(true);
+  };
+
+  const handleRegestration = () => {
+    setLogin(false);
+    setRegestration(true);
+  }
+
+
+  return (
+    <>
+      <div className="font-poppins min-h-screen w-full flex justify-center items-center">
+        <div className="bg-white h-[28rem]  w-[999.99px] rounded-xl flex justify-center items-center gap-[6.5rem] p-5">
+          <div className="md:hidden sm:hidden lg:block xl:block hidden">
+            <Image
+              src={vakilGiriAuth}
+              alt="Example"
+              height={1000}
+              width={1000}
+              />
+          </div>
+
+          {login && (
+            <Login handleForgetPassword={handleForgetPassword} handleRegestration={handleRegestration}/>
+          ) }
+
+          {forgetPassword && 
+          (
+            <ForgetPassword handleBackToLogin={handleBackToLogin}/>
+          )
+        }
+
+          {regestration && (
+            <Regester alreadyLogin={alreadyLogin}/>
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Form;
