d3d7941216b2a2a626468d0b5f3eb792be8255f1
Harsh Singh <hk5425901@gmail.com>
2024-12-24 21:12:47 +0530

    Add testing module and resolve conflicts

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,7 +5,7 @@ export async function middleware(request: NextRequest) {
   const token = await getToken({
     req: request,
     secret: process.env.NEXTAUTH_SECRET,
-    // cookieName: "__Secure-next-auth.session-token",
+    cookieName: "__Secure-next-auth.session-token",
   });
 
   const url = request.nextUrl;
